ed9612453280ba1f763c8b84a89ec3004f19c822
echo <enecholighten@163.com>
2025-04-10 10:38:21 +0100

    opencv camera test

--- main.cpp
+++ main.cpp
@@ -1,23 +1,37 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <vector>
 
 int main() {
-    cv::VideoCapture cap(0);
-    if (!cap.isOpened()) {
-        std::cerr << "无法打开摄像头" << std::endl;
+    FILE* pipe = popen("libcamera-vid -t 0 --codec yuv420 --width 640 --height 480 -o -", "r");
+    if (!pipe) {
+        std::cerr << "fail to start" << std::endl;
         return -1;
     }
 
-    cv::Mat frame;
-    while (true) {
-        cap >> frame;
-        if (frame.empty()) break;
+    int width = 640;
+    int height = 480;
+    int frame_size = width * height * 3 / 2;
 
-        cv::imshow("Camera", frame);
-        if (cv::waitKey(30) == 27) break;
+    std::vector<uchar> buffer(frame_size);
+    cv::Mat yuvImg(height + height / 2, width, CV_8UC1);
+    cv::Mat bgrImg;
+
+    while (true) {
+        size_t read_bytes = fread(buffer.data(), 1, frame_size, pipe);
+        if (read_bytes != frame_size) {
+            std::cerr << "fail 2" << std::endl;
+            break;
+        }
+
+        memcpy(yuvImg.data, buffer.data(), frame_size);
+        cv::cvtColor(yuvImg, bgrImg, cv::COLOR_YUV2BGR_I420);
+        cv::imshow("CSI Camera with OpenCV", bgrImg);
+
+        if (cv::waitKey(1) == 'q') break;
     }
 
-    cap.release();
-    cv::destroyAllWindows();
+    pclose(pipe);
     return 0;
 }
+
