cc5082c1d368a49bbe4a9c56cd1320d4bc3e2568
daibian <daibian@outlook.com>
2025-04-13 00:36:43 +0100

    添加新文件夹和文件

---
+++ build/CMakeFiles/FatigueNoVCam.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/helmet/Desktop/Open_cv_Fatigue_testing/main.cpp" "CMakeFiles/FatigueNoVCam.dir/main.cpp.o" "gcc" "CMakeFiles/FatigueNoVCam.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
